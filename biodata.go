733acd9faae99f1259dcaa720964a916bf502a89
Egi Puji Sutrisno <pujiegi6@gmail.com>
2024-03-05 22:35:18 +0700

    melakukan optimalisasi pada biodata.go

--- biodata.go
+++ biodata.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Biodata struct {
@@ -13,37 +14,46 @@ type Biodata struct {
 	Alasan    string
 }
 
-func findData(id string, data map[string]Biodata) (Biodata, error) {
-	//melakukan perulang untuk mencari apakah ada key yg sama dengan id
-	for key, value := range data {
-		if key == id {
-			//mereturn value dari key yang sama dan return error menjadi nil
-			return value, nil
-		}
+func findData(absen int, data map[int]Biodata) (*Biodata, error) {
+	//mencari siswa berdasarkan no absen
+	if value, ok := data[absen]; ok {
+		return &value, nil
 	}
 	//bila key tidak ditemukan mereturn value kosong/ struct kosong dan error message
-	return Biodata{}, errors.New("data siswa tidak ada")
+	return nil, errors.New("data siswa tidak ada")
 }
 
 func main() {
 	//mendapatkan argumen dari terminal
 	args := os.Args[1:]
 
-	siswaMap := map[string]Biodata{
-		"1": {Nama: "Egi Puji Sutrisno", Alamat: "Kota Tangerang", Pekerjaan: "Ingin menjadi backend programmer", Alasan: "Ingin mengasah skill golang"},
-		"2": {Nama: "Ilham", Alamat: "Jakarta", Pekerjaan: "Presiden", Alasan: "Mengasah skill"},
-		"3": {Nama: "Kurniawan", Alamat: "Bandung", Pekerjaan: "CEO", Alasan: "Ingin tau aja"},
-		"4": {Nama: "Kurniadi", Alamat: "Surabaya", Pekerjaan: "Pengusaha", Alasan: "Ingin kaya"},
+	if len(args) < 1 {
+		fmt.Println("Harap masukan nomer absen")
+		return
 	}
 
-	data, err := findData(args[0], siswaMap)
-
-	//cek apakah ada error
+	absen, err := strconv.Atoi(args[0])
 	if err != nil {
-		panic(err)
+		fmt.Println("Masukan absen berupa number")
+		return
 	}
 
-	fmt.Println("Data siswa dengan absen no", args[0])
+	siswaMap := map[int]Biodata{
+		1: {Nama: "Egi Puji Sutrisno", Alamat: "Kota Tangerang", Pekerjaan: "Ingin menjadi backend programmer", Alasan: "Ingin mengasah skill golang"},
+		2: {Nama: "Ilham", Alamat: "Jakarta", Pekerjaan: "Presiden", Alasan: "Mengasah skill"},
+		3: {Nama: "Kurniawan", Alamat: "Bandung", Pekerjaan: "CEO", Alasan: "Ingin tau aja"},
+		4: {Nama: "Kurniadi", Alamat: "Surabaya", Pekerjaan: "Pengusaha", Alasan: "Ingin kaya"},
+	}
+
+	data, err := findData(absen, siswaMap)
+
+	//mengecek apakah find data mereturn error atau error nil
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Data siswa dengan absen no", absen)
 	fmt.Println("Nama:", data.Nama)
 	fmt.Println("Alamat:", data.Alamat)
 	fmt.Println("Pekerjaan:", data.Pekerjaan)
